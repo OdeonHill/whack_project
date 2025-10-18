c97df22a1e4e759faf5341b9560fc82c49e183cd
DarthMorphus2 <darthmorphus@gmail.com>
2025-10-18 20:42:34 +0100

    linked all webpages together

--- website_code/views.py
+++ website_code/views.py
@@ -9,3 +9,27 @@ def home():
 @views.route('/dashboard')
 def dashboard():
     return render_template("dashboard.html")
+
+@views.route('/lessons')
+def lessons():
+    return render_template("lessons.html")
+
+@views.route('/tracker')
+def tracker():
+    return render_template("tracker.html")
+
+@views.route('/investing_lesson')
+def investing_lesson():
+    return render_template("investing_lesson.html")
+
+@views.route('/phishing_lesson')
+def phishing_lesson():
+    return render_template("phishing_lesson.html")
+
+@views.route('/savings_lesson')
+def savings_lesson():
+    return render_template("savings_lesson.html")
+
+@views.route('/login')
+def login():
+    return render_template("login.html")
