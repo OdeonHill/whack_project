f042b635677e04cd013dbf20c4ff1ea3046c5def
DarthMorphus2 <darthmorphus@gmail.com>
2025-10-18 12:31:55 +0100

    deleted old init, created basic base.html

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from werkzeug.security import generate_password_hash
-from dotenv import load_dotenv
-import os
-from os import path
